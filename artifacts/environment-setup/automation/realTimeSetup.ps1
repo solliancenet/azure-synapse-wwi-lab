cdbf1e39256a8e6ad4da7506582f5e1031f121d9
xor-ranavikrant <33507675+xor-ranavikrant@users.noreply.github.com>
2020-07-15 15:33:46 +0530

    updates made to script and templates

--- artifacts/environment-setup/automation/realTimeSetup.ps1
+++ artifacts/environment-setup/automation/realTimeSetup.ps1
@@ -2,13 +2,13 @@ az login
 Install-Module -Name MicrosoftPowerBIMgmt
 Login-PowerBI
 $subscription=Read-Host "Enter subscription name from above list"
-$asaName=Read-Host "Enter ASA name from the created resource"
-$wsId=Read Host "Enter your powerBi workspace Id"
-$TwitterAccessToken=Read Host "Enter your twitter access token"
-$TwitterAccessTokenSecret=Read Host "Enter your twitter access token secret"
-$TwitterConsumerKey=Read Host "Enter your twitter consumer key"
-$TwitterConsumerKeySecret=Read Host "Enter your twitter consumer key secret"
-$TwitterKeywords=Read Host "Enter keywords in hashtags to be monitored with ,(comma) seperation"
+
+$wsId=Read-Host "Enter your powerBi workspace Id"
+$TwitterAccessToken=Read-Host "Enter your twitter access token"
+$TwitterAccessTokenSecret=Read-Host "Enter your twitter access token secret"
+$TwitterConsumerKey=Read-Host "Enter your twitter consumer key"
+$TwitterConsumerKeySecret=Read-Host "Enter your twitter consumer key secret"
+$TwitterKeywords=Read-Host "Enter keywords in hashtags to be monitored with ,(comma) seperation"
 az account set --subscription $subscription
 $subscriptionId=az account show|ConvertFrom-Json
 
@@ -30,9 +30,24 @@ $streamingDatasetUrl= Read-Host "Please enter streaming dataset url for Location
 				-replace '#TwitterKeywords#', $TwitterKeywords`
         } | Set-Content -Path TwitterARM.json
 	
-az group deployment create --resource-group $resourceGroup --template-file ./TwitterARM.json  #deploys azure function at specified resource-group	
+az group deployment create --resource-group $resourceGroup --template-file ./TwitterARM.json >> output.txt #deploys azure function at specified resource-group	
+$output_json = Get-Content "output.txt" | Out-String | ConvertFrom-Json
+$twitterFunction=$output_json.properties.outputs.twitter_azure_function_functions_app_name
+$locationFunction=$output_json.properties.outputs.location_azure_function_functions_app_name
+$asaName=$output_json.properties.outputs.asaName
 
-$principal=az resource show --ids /subscriptions/subscriptionId.id/resourceGroups/$resourceGroup/providers/Microsoft.StreamAnalytics/StreamingJobs/$asaName |ConvertFrom-Json
+az functionapp deployment source config-zip `
+        --resource-group $resourceGroup `
+        --name $twitterFunction `
+        --src "../functions/powershell-functions/Twitter_Function_Publish_Package.zip"
+		
+az functionapp deployment source config-zip `
+        --resource-group $resourceGroup `
+        --name $locationFunction `
+        --src "functions/powershell-functions/LocationAnalytics_Publish_Package.zip"
+
+# $principal=az resource show --ids /subscriptions/$subscriptionId.id/resourceGroups/$resourceGroup/providers/Microsoft.StreamAnalytics/StreamingJobs/$asaName |ConvertFrom-Json
+$principal=az resource show -g test-realtime-rana -n TweetsASA --resource-type "Microsoft.StreamAnalytics/streamingjobs"|ConvertFrom-Json
 $principalId=$principal.identity.principalId
 Add-PowerBIWorkspaceUser -WorkspaceId $wsId -PrincipalId $principalId -PrincipalType App -AccessRight Contributor
 
